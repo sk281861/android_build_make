ed69faafe7c0cd82ec30adf2a07e355cb5d445b8
Simao Gomes Viana <xdevs23@outlook.com>
2017-08-24 17:41:53 +0200

    Support for global Make variables in soong [2/2]
    Export the environment variable TARGET_DEVICE_DIR so that it can be
    accessed in Go later.
    
    Change-Id: I280f57958908c42682360f5cc8caba0b88a65a97

--- envsetup.sh
+++ envsetup.sh
@@ -1611,6 +1611,7 @@ function get_make_command()
 
 function make()
 {
+    export TARGET_DEVICE_DIR="$(find device/ -maxdepth 2 -name ${TARGET_PRODUCT/XOS_/} -type d)"
     local start_time=$(date +"%s")
     $(get_make_command) "$@"
     local ret=$?
