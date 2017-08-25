ce6ef424e9681463e7099f3d0da9b5eb82ade23a
Simao Gomes Viana <xdevs23@outlook.com>
2017-08-26 00:08:57 +0200

    core: config: only include our vendor sepolicy if exists
    Recently nuked
    
    Change-Id: I246031d78232b679b1c38f60a460332b1442d0a6

--- core/config.mk
+++ core/config.mk
@@ -894,7 +894,7 @@ endif
 ifneq ($(XOS_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/xos/sepolicy/sepolicy.mk)
+-include vendor/xos/sepolicy/sepolicy.mk
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
